512b0f56a3f905f191c2b62001dbaa61adfe49e0
Nicholas Jacob <nicholas.jacob@guitarcenter.com>
2021-08-12 15:08:37 -0700

    Flexbox and layouts (intro)

--- App.js
+++ App.js
@@ -4,10 +4,13 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{padding: 50}}>
-      <View>
-        <TextInput placeholder="Course Goal" style={{borderColor: 'black', borderWidth: 1, padding: 10}} />
-        <Button title="ADD" />
+    <View style={{ padding: 50 }}>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItem: 'center' }}>
+        <TextInput
+          placeholder='Course Goal'
+          style={{ width: '80%', borderColor: 'black', borderWidth: 1, padding: 10 }}
+        />
+        <Button title='ADD' />
       </View>
       <View></View>
     </View>
